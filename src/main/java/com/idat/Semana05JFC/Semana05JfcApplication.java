4ff8bfdb38d13ca2d72668dc7b73a84a6c461b69
JairFercas <jair1162@hotmail.com>
2024-06-28 14:31:02 -0500

    Creacion del proyecto semana 5

---
+++ src/main/java/com/idat/Semana05JFC/Semana05JfcApplication.java
@@ -0,0 +1,13 @@
+package com.idat.Semana05JFC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semana05JfcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Semana05JfcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idat/Semana05JFC/Semana05JfcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.Semana05JFC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semana05JfcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
